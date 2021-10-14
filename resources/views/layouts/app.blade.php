337f3621aec2a86ed7a568c782d2ef6665144717
nahufede <nahuelbarbieri.93@gmail.com>
2021-10-14 17:33:12 +0200

    building assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <title>Lumo</title>
 </head>
 <body>
@@ -12,6 +12,6 @@
     <div class="container-fluid" style="padding: 184px 0">
         @yield('content')
     </div>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
